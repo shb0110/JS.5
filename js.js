956a9a7b59344e23e093382ac46bb1957404d476
shb0110 <99003926+shb0110@users.noreply.github.com>
2022-04-25 11:00:08 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,54 @@
+const oy =prompt(`Qaysi oyda tug'ulgan siz..`)
+switch(Number(oy)){
+    case 1:
+        alert("Yanvar")
+        document.write("<div class=shb1 >Yanvar</div>")
+    break;
+    case 2:
+        alert("Fevral")
+        document.write("<div class=shb2 >Fevral</div>")
+    break;
+    case 3:
+        alert("Mart")
+        document.write("<div class=shb3 >Mart</div>")
+    break;
+    case 4:
+        alert("Aprel")
+        document.write("<div class=shb4 >Aprel</div>")
+    break;
+    case 5:
+        alert("May")
+        document.write("<div class=shb5 >May</div>")
+    break;
+    case 6:
+        alert("Ilun")
+        document.write("<div class=shb6 >Ilun</div>")
+    break;
+    case 7:
+        alert("Ilul")
+        document.write("<div class=shb7 >Ilul</div>")
+    break;
+    case 8:
+        alert("Avgust")
+        document.write("<div class=shb8  >Avgust</div>")
+    break;
+    case 9:
+        alert("Sentiabr")
+        document.write("<div class=shb9 >Sentabr</div>")
+    break;
+    case 10:
+        alert("Oktyabr")
+        document.write("<div class=shb10 >Oktabr</div>")
+    break;
+    case 11:
+        alert("Noyabr")
+        document.write("<div class=shb11 >Noyabr</div>")
+    break;
+    case 12:
+        alert("Dekabr")
+        document.write("<div class=shb12 >Dekabr</div>")
+    break;
+    default:alert("Bunday oy mayjut emas")
+    break;
+}
+console.log(typeof (kun))
